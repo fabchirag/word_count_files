0d97e36919b7cdcf0664e752ef24bfdb0ae476e7
fabchirag <chiraglimbachia@gmail.com>
2021-01-10 09:56:46 +0000

    function that counts words from the files

---
+++ word_count.py
@@ -0,0 +1,15 @@
+def count_words(filename):
+    """Count the approximate number of words in a file"""
+    try:
+        with open(filename, encoding="utf-8") as f:
+            contents = f.read()
+    except FileNotFoundError:
+        print(f"Sorry, the file {filename} does not exist.")
+    else:
+        words = contents.split()
+        num_words = len(words)
+        print(f"The file {filename} has about {num_words} words.")
+
+file_names = ["moby_dick.txt", "alice.txt", "little_women.txt", "siddhartha.txt", "revange.txt"]
+for file_name in file_names:
+    count_words(file_name)
